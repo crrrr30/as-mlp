462400b487c48bf1ec62d169868bcfa1fab848df
crrrr30 <jonathancui03@gmail.com>
2023-06-11 19:06:59 -0400

    Update

--- data/build.py
+++ data/build.py
@@ -31,42 +31,13 @@ def mixup_target(target, num_classes, lam=1., smoothing=0.0, device='cuda'):
 
 
 def fast_collate(batch):
-    """ A fast collation function optimized for uint8 images (np array or torch) and int64 targets (labels)"""
-    if isinstance(batch[0], dict):
-        batch = [(b["image"], b["label"]) for b in batch]
-    assert isinstance(batch[0], tuple)
     batch_size = len(batch)
-    if isinstance(batch[0][0], tuple):
-        # This branch 'deinterleaves' and flattens tuples of input tensors into one tensor ordered by position
-        # such that all tuple of position n will end up in a torch.split(tensor, batch_size) in nth position
-        inner_tuple_size = len(batch[0][0])
-        flattened_batch_size = batch_size * inner_tuple_size
-        targets = torch.zeros(flattened_batch_size, dtype=torch.int64)
-        tensor = torch.zeros((flattened_batch_size, *batch[0][0][0].shape), dtype=torch.uint8)
-        for i in range(batch_size):
-            assert len(batch[i][0]) == inner_tuple_size  # all input tensor tuples must be same length
-            for j in range(inner_tuple_size):
-                targets[i + j * batch_size] = batch[i][1]
-                tensor[i + j * batch_size] += torch.from_numpy(batch[i][0][j])
-        return tensor, targets
-    elif isinstance(batch[0][0], np.ndarray):
-        print("numpy array")
-        targets = torch.tensor([b[1] for b in batch], dtype=torch.int64)
-        assert len(targets) == batch_size
-        tensor = torch.zeros((batch_size, *batch[0][0].shape), dtype=torch.uint8)
-        for i in range(batch_size):
-            tensor[i] += torch.from_numpy(batch[i][0])
-        return tensor, targets
-    elif isinstance(batch[0][0], torch.Tensor):
-        print("pytorch tensor")
-        targets = torch.tensor([b[1] for b in batch], dtype=torch.int64)
-        assert len(targets) == batch_size
-        tensor = torch.zeros((batch_size, *batch[0][0].shape), dtype=torch.uint8)
-        for i in range(batch_size):
-            tensor[i].copy_(batch[i][0])
-        return tensor, targets
-    else:
-        assert False
+    targets = torch.tensor([b["label"] for b in batch], dtype=torch.int64)
+    assert len(targets) == batch_size
+    tensor = torch.zeros((batch_size, *batch[0]["image"].shape), dtype=torch.float32)
+    for i in range(batch_size):
+        tensor[i].copy_(batch[i]["image"])
+    return tensor, targets
 
 
 class FastCollateMixup(Mixup):
