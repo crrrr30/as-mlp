eb90329fbdcc496c3c76c15cf4b252547b5dcc0b
crrrr30 <jonathancui03@gmail.com>
2023-06-11 16:25:59 -0400

    Update

--- data/build.py
+++ data/build.py
@@ -18,6 +18,11 @@ from .samplers import SubsetRandomSampler
 from datasets import concatenate_datasets, Dataset
 
 
+def one_hot(x, num_classes, on_value=1., off_value=0., device='cuda'):
+    x = x.long().view(-1, 1)
+    return torch.full((x.size()[0], num_classes), off_value, device=device).scatter_(1, x, on_value)
+
+
 def mixup_target(target, num_classes, lam=1., smoothing=0.0, device='cuda'):
     off_value = smoothing / num_classes
     on_value = 1. - smoothing + off_value
